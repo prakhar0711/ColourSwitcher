ac8ea37580a0f89ab849368644015ab08692fdac
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-04 13:32:22 +0530

    Initial commit

---
+++ colourchange.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll(".button");
+// console.log(buttons);
+const body = document.querySelector("body");
+
+buttons.forEach((button) => {
+  //   console.log(button);
+  button.addEventListener("click", function (event) {
+    // console.log(event);
+    // console.log(event.target);
+    if (event.target.id === "grey") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "white") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "blue") {
+      body.style.backgroundColor = event.target.id;
+    } else if (event.target.id === "yellow") {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
